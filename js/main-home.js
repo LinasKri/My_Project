fdd6285f20217bd018c7a3b4e4acbef884341e65
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-11 20:06:40 +0300

    JS comentarai

---
+++ js/main-home.js
@@ -0,0 +1,41 @@
+// IMPORTS
+/* header */
+
+/* hero */
+
+/* about me */
+
+/* acievements */
+
+/* services */
+
+/* hobies */
+
+/* portfolio */
+
+/* testamonials */
+
+/* contact me */
+
+/* footer */
+
+//EXECUTION
+/* header */
+
+/* hero */
+
+/* about me */
+
+/* acievements */
+
+/* services */
+
+/* hobies */
+
+/* portfolio */
+
+/* testamonials */
+
+/* contact me */
+
+/* footer */
